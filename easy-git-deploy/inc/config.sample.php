a3fc13577970191b321b5fa0a898a57a30ed6341
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-01-08 18:51:30 +0100

    a short explanation for config.sample.php

--- easy-git-deploy/inc/config.sample.php
+++ easy-git-deploy/inc/config.sample.php
@@ -2,12 +2,14 @@
 /* rename this file to config.php! */
 
 $config = array(
-    // The directory where your website and git repository are located, can be
-    // a relative or absolute path, will be relative to the inc/ folder!
+
+    // The directory where your website and git repository are located, can be a relative or absolute path
     'directory' => '../my_repo',
 
     // Used for first clone
     'url' => 'https://USER:PASS@bitbucket.org/account/repo.git'
+
+    // Further options: 'log', 'date_format', 'branch', 'remote'
 );
 
 
