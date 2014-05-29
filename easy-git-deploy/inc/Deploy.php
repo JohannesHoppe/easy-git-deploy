bbfde5d14a83012cd1b11fead3bbb18dbc32189b
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-05-29 13:31:27 +0200

    i hate whitespaces

--- easy-git-deploy/inc/Deploy.php
+++ easy-git-deploy/inc/Deploy.php
@@ -1,62 +1,62 @@
 <?php
 
 class Deploy extends VcsAbstract {
-	/**
-	 * Extracts the host of git@bitbucket.org:xxx/xxx
-	 */
-	/*
-	private function extract_host_from_git_url($url) {
-		$matches = null;
-		preg_match('/git@([^:]+):/',$url, $matches);
-		return $matches[1];
-	}
-	*/
+    /**
+     * Extracts the host of git@bitbucket.org:xxx/xxx
+     */
+    /*
+    private function extract_host_from_git_url($url) {
+        $matches = null;
+        preg_match('/git@([^:]+):/',$url, $matches);
+        return $matches[1];
+    }
+    */
 
-	/**
-	 * Executes the necessary commands to deploy the website.
-	 */
-	public function execute() {
-		try {
-			if (!$this->has_base_directory()) {
-				mkdir($this->_directory, 0777, true);
-			}
+    /**
+     * Executes the necessary commands to deploy the website.
+     */
+    public function execute() {
+        try {
+            if (!$this->has_base_directory()) {
+                mkdir($this->_directory, 0777, true);
+            }
 
-			// Make sure we're in the right directory
-			chdir($this->_directory);
+            // Make sure we're in the right directory
+            chdir($this->_directory);
 
-			if (!is_dir('./.git')) {
-				// only first run
+            if (!is_dir('./.git')) {
+                // only first run
 
-				// TODO: finish ssh implementation
-				//$host = $this->extract_host_from_git_url($this->_url);
-				//$this->exec_and_log('Adding remote server to list of known hosts.', 'ssh '.$host.' -o StrictHostKeyChecking=no ');
+                // TODO: finish ssh implementation
+                //$host = $this->extract_host_from_git_url($this->_url);
+                //$this->exec_and_log('Adding remote server to list of known hosts.', 'ssh '.$host.' -o StrictHostKeyChecking=no ');
 
-				// The "." at the end specifies the current folder as the checkout folder!
-				$this->exec_and_log('Cloning repo for the first time.', "git clone '" . $this->_url . "' .");
-			}
-			else {
-				// normal run
+                // The "." at the end specifies the current folder as the checkout folder!
+                $this->exec_and_log('Cloning repo for the first time.', "git clone '" . $this->_url . "' .");
+            }
+            else {
+                // normal run
 
-				// Move to master branch.
-				$this->exec_and_log('Switching to master branch.', 'git checkout ' . $this->_branch);
+                // Move to master branch.
+                $this->exec_and_log('Switching to master branch.', 'git checkout ' . $this->_branch);
 
-				// Move to master branch.
-				$this->exec_and_log('Cleaning master branch.', 'git reset --hard');
+                // Move to master branch.
+                $this->exec_and_log('Cleaning master branch.', 'git reset --hard');
 
-				// Update the local repository
-				$this->exec_and_log('Pulling in changes.', 'git pull ' . $this->_remote . ' ' . $this->_branch);
-			}
+                // Update the local repository
+                $this->exec_and_log('Pulling in changes.', 'git pull ' . $this->_remote . ' ' . $this->_branch);
+            }
 
-			// Secure the .git directory
-			$this->exec_and_log('Securing .git directory.', 'chmod -R og-rx .git');
+            // Secure the .git directory
+            // $this->exec_and_log('Securing .git directory.', 'chmod -R og-rx .git');
 
-			// Execute post deploy callback.
-			$this->post_deploy();
+            // Execute post deploy callback.
+            $this->post_deploy();
 
-			$this->log('Deployment successful.');
-		}
-		catch (Exception $e) {
-			$this->log($e, 'ERROR');
-		}
-	}
+            $this->log('Deployment successful.');
+        }
+        catch (Exception $e) {
+            $this->log($e, 'ERROR');
+        }
+    }
 }
--- easy-git-deploy/inc/VcsAbstract.php
+++ easy-git-deploy/inc/VcsAbstract.php
@@ -1,133 +1,133 @@
 <?php
 
 abstract class VcsAbstract implements VcsInterface {
-	/**
-	 * A callback function to call after the deploy has finished.
-	 *
-	 * @var callback
-	 */
-	public $post_deploy;
+    /**
+     * A callback function to call after the deploy has finished.
+     *
+     * @var callback
+     */
+    public $post_deploy;
 
-	/**
-	 * The name of the file that will be used for logging deployments. Set to
-	 * FALSE to disable logging.
-	 *
-	 * @var string
-	 */
-	private $_log = '../deployments.log';
+    /**
+     * The name of the file that will be used for logging deployments. Set to
+     * FALSE to disable logging.
+     *
+     * @var string
+     */
+    private $_log = '../deployments.log';
 
-	/**
-	 * All logged messages of the current run
-	 *
-	 * @var string
-	 */
-	public $_log_messages = '';
+    /**
+     * All logged messages of the current run
+     *
+     * @var string
+     */
+    public $_log_messages = '';
 
-	/**
-	 * The timestamp format used for logging.
-	 *
-	 * @link    http://www.php.net/manual/en/function.date.php
-	 * @var     string
-	 */
-	private $_date_format = 'Y-m-d H:i:sP';
+    /**
+     * The timestamp format used for logging.
+     *
+     * @link    http://www.php.net/manual/en/function.date.php
+     * @var     string
+     */
+    private $_date_format = 'Y-m-d H:i:sP';
 
-	/**
-	 * The name of the branch to pull from.
-	 *
-	 * @var string
-	 */
-	protected $_branch = 'master';
+    /**
+     * The name of the branch to pull from.
+     *
+     * @var string
+     */
+    protected $_branch = 'master';
 
-	/**
-	 * The name of the remote to pull from.
-	 *
-	 * @var string
-	 */
-	protected $_remote = 'origin';
+    /**
+     * The name of the remote to pull from.
+     *
+     * @var string
+     */
+    protected $_remote = 'origin';
 
-	/**
-	 * The directory where your website and git repository are located, can be
-	 * a relative or absolute path, will be relative to the inc/ folder
-	 *
-	 * @var string
-	 */
-	protected $_directory;
+    /**
+     * The directory where your website and git repository are located, can be
+     * a relative or absolute path, will be relative to the inc/ folder
+     *
+     * @var string
+     */
+    protected $_directory;
 
-	/**
-	 * Only used for first clone
-	 */
-	protected $_url;
+    /**
+     * Only used for first clone
+     */
+    protected $_url;
 
-	/**
-	 * Sets up defaults.
-	 *
-	 * @param  array $config  Information about the deployment
-	 */
-	public function __construct($config = array()) {
-		$available_options = array('directory', 'log', 'date_format', 'branch', 'remote', 'url');
+    /**
+     * Sets up defaults.
+     *
+     * @param  array $config  Information about the deployment
+     */
+    public function __construct($config = array()) {
+        $available_options = array('directory', 'log', 'date_format', 'branch', 'remote', 'url');
 
-		foreach ($config as $option => $value) {
-			if (in_array($option, $available_options)) {
-				$this->{'_' . $option} = $value;
-			}
-		}
+        foreach ($config as $option => $value) {
+            if (in_array($option, $available_options)) {
+                $this->{'_' . $option} = $value;
+            }
+        }
 
-		$this->log('Attempting deployment...');
-	}
+        $this->log('Attempting deployment...');
+    }
 
-	/**
-	 * Writes a message to the log file.
-	 *
-	 * @param  array|string $message  The message(s) to write
-	 * @param  string $type     The type of log message (e.g. INFO, DEBUG, ERROR, etc.)
-	 */
-	public function log($message, $type = 'INFO') {
-		if ($this->_log) {
-			// Set the name of the log file
-			$filename = $this->_log;
+    /**
+     * Writes a message to the log file.
+     *
+     * @param  array|string $message  The message(s) to write
+     * @param  string $type     The type of log message (e.g. INFO, DEBUG, ERROR, etc.)
+     */
+    public function log($message, $type = 'INFO') {
+        if ($this->_log) {
+            // Set the name of the log file
+            $filename = $this->_log;
 
-			if (!file_exists($filename)) {
-				// Create the log file
-				file_put_contents($filename, '');
+            if (!file_exists($filename)) {
+                // Create the log file
+                file_put_contents($filename, '');
 
-				// Allow anyone to write to log files
-				chmod($filename, 0666);
-			}
+                // Allow anyone to write to log files
+                chmod($filename, 0666);
+            }
 
-			if (is_array($message)) {
-				$message = implode("\n\t", $message);
-			}
+            if (is_array($message)) {
+                $message = implode("\n\t", $message);
+            }
 
-			$formatted = date($this->_date_format) . ' --- ' . $type . ': ' . $message . "\n";
-			file_put_contents($filename, $formatted, FILE_APPEND);
+            $formatted = date($this->_date_format) . ' --- ' . $type . ': ' . $message . "\n";
+            file_put_contents($filename, $formatted, FILE_APPEND);
 
-			$this->_log_messages .= $formatted;
-		}
-	}
+            $this->_log_messages .= $formatted;
+        }
+    }
 
-	/**
-	 * Calls exec and logs its input and output
-	 * @param string $description  The description of the command
-	 * @param string $command      The command to execute
-	 */
-	public function exec_and_log($description, $command) {
-		$output = array();
-		exec($command . ' 2>&1', $output);
+    /**
+     * Calls exec and logs its input and output
+     * @param string $description  The description of the command
+     * @param string $command      The command to execute
+     */
+    public function exec_and_log($description, $command) {
+        $output = array();
+        exec($command . ' 2>&1', $output);
 
-		$messages = array_merge(array($description, $command), $output);
-		$this->log($messages);
-	}
+        $messages = array_merge(array($description, $command), $output);
+        $this->log($messages);
+    }
 
-	/**
-	 * @return bool
-	 */
-	protected function has_base_directory() {
-		return file_exists($this->_directory);
-	}
+    /**
+     * @return bool
+     */
+    protected function has_base_directory() {
+        return file_exists($this->_directory);
+    }
 
-	protected function post_deploy() {
-		if (is_callable($this->post_deploy)) {
-			call_user_func($this->post_deploy);
-		}
-	}
+    protected function post_deploy() {
+        if (is_callable($this->post_deploy)) {
+            call_user_func($this->post_deploy);
+        }
+    }
 }
