b02669e6d65f7dcbd1131eadfb849ff80be2d975
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2017-04-16 14:07:03 +0200

    suppress annoying PHP 5.3 error
    silently ignores "date(): It is not safe to rely on the system's timezone settings..." as seen on stackoverflow:
    http://stackoverflow.com/a/19922408/2042765

--- easy-git-deploy/bootstrap.php
+++ easy-git-deploy/bootstrap.php
@@ -5,6 +5,11 @@ header("Content-Type:text/plain");
 ini_set('display_errors', 1);
 error_reporting(E_ALL + E_STRICT + E_DEPRECATED);
 
+// Suppress error: date(): It is not safe to rely on the system's timezone settings...
+if(!ini_get('date.timezone')) {
+  date_default_timezone_set('GMT');
+}
+
 require_once './inc/config.php';
 require_once './inc/VcsInterface.php';
 require_once './inc/VcsAbstract.php';
